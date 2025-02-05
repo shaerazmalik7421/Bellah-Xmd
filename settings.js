141a176269f1c1db0b3422d0e86bccf381e3b2e0
shaerazmalik7421 <165659336+shaerazmalik7421@users.noreply.github.com>
2025-02-05 14:16:16 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~mvZmWbRR#3ddLf_E_ChqG4TAcGBApCMemf6tGxBvu2o_JPLAQ6e8' 
 //owmner v card
 global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
 global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
